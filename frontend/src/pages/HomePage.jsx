ee16748de8746cd268ca375e94526d42a1560ff0
Wesley Lui <wesley.lui2@ucalgary.ca>
2025-02-11 16:14:44 -0700

    installed tailwindcss, updated homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 const HomePage = () => {
   return (
-    <div>
-        <h1>nvtworks</h1>
+    <div className="bg-dark-blue text-white p-6 rounded shadow-lg">
+      <h2 className="text-xl font-bold">Login</h2>
+      <input
+        type="text"
+        className="border p-2 w-full mt-4 bg-dark-blue text-white"
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        className="border p-2 w-full mt-2 bg-dark-blue text-white"
+        placeholder="Password"
+      />
+      <button className="bg-blue-500 text-white p-2 rounded w-full mt-4">
+        Login
+      </button>
     </div>
   );
 };
