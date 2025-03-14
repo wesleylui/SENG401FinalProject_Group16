a4ff665e011acc1f5c0df893f3fe01288a7024ab
Wesley Lui <wesley.lui2@ucalgary.ca>
2025-03-13 18:29:16 -0600

    profile icon only when signed in. also testing frontend deployment updates correctly

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,35 +9,37 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-400 p-4 shadow-md z-50 flex justify-between items-center">
-      <Link to="/main">
-        <h1 className="text-white hover:text-gray-700 transition duration-300">
-          S.T.O.R.Y
-        </h1>
-      </Link>
+      <h1 className="text-white hover:text-gray-700 transition duration-300">
+        S.T.O.R.Y
+      </h1>
       <div className="flex justify-center w-full">
         {location.pathname !== "/" && location.pathname !== "/signup" && (
-          <Link to="/main">
-            <button className="text-black p-2 rounded w-full mt-3 hover transition mr-4">
-              New Story
-            </button>
-          </Link>
-        )}
-        {isAuthenticated && !isGuest && (
-          <Link to="/saved-stories">
-            <button className="text-black p-2 rounded w-full mt-3 hover transition ml-4">
-              View Saved Stories
-            </button>
-          </Link>
+          <>
+            <Link to="/main">
+              <button className="text-black p-2 rounded w-full mt-3 hover transition mr-4">
+                New Story
+              </button>
+            </Link>
+            {!isGuest && (
+              <Link to="/saved-stories">
+                <button className="text-black p-2 rounded w-full mt-3 hover transition ml-4">
+                  View Saved Stories
+                </button>
+              </Link>
+            )}
+          </>
         )}
       </div>
       <div className="flex items-center ml-4 pr-4">
-        <Link to="/profile">
-          <FontAwesomeIcon
-            icon={faUser}
-            size="2x"
-            className="text-white p-2 rounded hover:text-gray-700 transition duration-300"
-          />
-        </Link>
+        {location.pathname !== "/" && location.pathname !== "/signup" && (
+          <Link to="/profile">
+            <FontAwesomeIcon
+              icon={faUser}
+              size="2x"
+              className="text-white p-2 rounded hover:text-gray-700 transition duration-300"
+            />
+          </Link>
+        )}
       </div>
     </header>
   );
