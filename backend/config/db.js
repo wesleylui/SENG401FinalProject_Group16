0a6ceff4812a11a1d66f7cacc48ddd4ad33b97eb
Farhan-Abbas <fabbas.ambition@gmail.com>
2025-03-18 10:54:38 -0600

    changed db.js to use a local database for testing.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,17 +2,17 @@ const mysql = require("mysql2");
 require("dotenv").config(); // load .env variables
 
 // Create a MySQL connection (locally)
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: process.env.DB_PASSWORD, // no pw. may have to change your sql pw (ask wesley)
-//   database: "story_gen", // db is called story_gen
-// });
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD, // no pw. may have to change your sql pw (ask wesley)
+  database: "story_gen", // db is called story_gen
+});
 
-console.log("Connecting to:", process.env.DATABASE_URL);
+// console.log("Connecting to:", process.env.DATABASE_URL);
 
 // create MYSQL connection using Railway database URL
-const db = mysql.createConnection(process.env.DATABASE_URL);
+// const db = mysql.createConnection(process.env.DATABASE_URL);
 
 // Connect to MySQL
 db.connect((err) => {
