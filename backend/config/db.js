37aafe0fc595b9046f291d2cb6dd7558734ce80d
Wesley Lui <wesley.lui2@ucalgary.ca>
2025-03-19 21:41:33 -0600

    fix env var names and npm install mysql

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 require("dotenv").config(); // Load .env variables
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
 
